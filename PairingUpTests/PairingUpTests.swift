1fbca2fafd40d6da9471b80b47a187adeb6c75e6
michael guatambu davis <guatambu@dundak.com>
2019-05-05 20:32:36 -0700

    Initialize repository

---
+++ PairingUpTests/PairingUpTests.swift
@@ -0,0 +1,34 @@
+//
+//  PairingUpTests.swift
+//  PairingUpTests
+//
+//  Created by Kelly Johnson on 5/5/19.
+//  Copyright © 2019 DunDak, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import PairingUp
+
+class PairingUpTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
